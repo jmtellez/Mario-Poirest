2d42dc80fbf56536da08f0ab1f5fddff23ba8d4b
Juan Tellez <juan.tellez@aa.com>
2021-01-22 15:52:27 -0600

    adding Swagger annotations

--- src/main/java/com/boyz/mariopoirest/REST/MarioRestController.java
+++ src/main/java/com/boyz/mariopoirest/REST/MarioRestController.java
@@ -9,18 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class MarioRestController {
 
     @Autowired
     MarioService marioService;
 
+    @ApiOperation(value = "Get Mario by Id", response = byte.class)
     @GetMapping(value = "/mario/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getPictureById(@PathVariable int id) {
 
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(marioService.getMarioBytesById(id));
     }
-
+    @ApiOperation(value = "Get random Mario", response = byte.class)
     @GetMapping(value = "/mario/random", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getRandomPicture() {
 
